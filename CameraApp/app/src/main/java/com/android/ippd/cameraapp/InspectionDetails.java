b5fea179fd1e61585f1f6484d9d4aaa26a30f3c9
Nicole Kulakowski <kulakowskin@yahoo.com>
2015-10-21 17:35:50 -0400

    InspectionDetails.java

---
+++ CameraApp/app/src/main/java/com/android/ippd/cameraapp/InspectionDetails.java
@@ -0,0 +1,27 @@
+package com.android.ippd.cameraapp;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+
+/**
+ * Created by MyMac on 10/21/15.
+ */
+public class InspectionDetails extends MainActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_inspection);
+
+        // for parts dropdown, will list parts previously created as well as option to create new part
+        Spinner dropdown = (Spinner)findViewById(R.id.partsSpinner);
+        String[] items = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+    }
+}
