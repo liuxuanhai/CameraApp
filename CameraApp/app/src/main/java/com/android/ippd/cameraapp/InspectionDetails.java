01c4c05a08ff6bd53401d83ca26db075c93983f4
mbentz <mbentz@ufl.edu>
2015-10-30 12:31:23 -0400

    Added up navigation to main activity

--- CameraApp/app/src/main/java/com/android/ippd/cameraapp/InspectionDetails.java
+++ CameraApp/app/src/main/java/com/android/ippd/cameraapp/InspectionDetails.java
@@ -2,7 +2,10 @@ package com.android.ippd.cameraapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 /**
@@ -10,15 +13,29 @@ import android.widget.Spinner;
  */
 public class InspectionDetails extends Activity {
 
+    private Button backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details_inspection);
+
+        // Hide soft keys
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
 
         // for parts dropdown, will list parts previously created as well as option to create new part
         Spinner dropdown = (Spinner)findViewById(R.id.partsSpinner);
         String[] items = new String[]{"1", "2", "three"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
+
+
+        backButton = (Button)findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                NavUtils.navigateUpFromSameTask(InspectionDetails.this);
+            }
+        });
     }
 }
